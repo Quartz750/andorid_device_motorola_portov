e60dbf41844f7a38ec9edeac9fe19565ccf34f25
Quartz750 <430639454@qq.com>
2026-09-08 14:32:01 +0800

    portov: Use prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,7 @@
 #
 
 DEVICE_PATH := device/motorola/portov
+KERNEL_PATH := $(DEVICE_PATH)-kernel
 
 # Inherit from motorola sm6450-common
 include device/motorola/sm6450-common/BoardConfigCommon.mk
@@ -12,7 +13,51 @@ include device/motorola/sm6450-common/BoardConfigCommon.mk
 TARGET_BOOTLOADER_BOARD_NAME := portov
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/ext_config/moto-parrot-portov.config
+BOARD_KERNEL_CMDLINE += \
+    printk.devkmsg=on \
+    firmware_class.path=/vendor/firmware_mnt/image \
+    androidboot.selinux=permissive \
+    sysctl.kernel.firmware_config.force_sysfs_fallback=1
+
+BOARD_BOOTCONFIG := \
+    androidboot.hardware=qcom \
+    androidboot.hypervisor.protected_vm.supported=true \
+    androidboot.load_modules_parallel=true \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=a600000.dwc3 \
+    androidboot.vendor.qspa=true
+
+BOARD_USES_GENERIC_KERNEL_IMAGE := true
+BOARD_KERNEL_BASE := 0x00000000
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_IMAGE_NAME := Image
+
+# Prebuilt kernel
+BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
+BOARD_RAMDISK_OFFSET     := 0x02000000
+
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
+TARGET_KERNEL_VERSION := 6.6
+TARGET_NO_KERNEL_OVERRIDE := true
+
+PRODUCT_COPY_FILES += \
+    $(KERNEL_PATH)/kernel:kernel
+
+# Kernel modules
+DLKM_MODULES_PATH := $(KERNEL_PATH)/vendor_dlkm
+RAMDISK_MODULES_PATH := $(KERNEL_PATH)/vendor_ramdisk
+SYSTEM_DLKM_MODULES_PATH := $(KERNEL_PATH)/system_dlkm
+
+BOARD_SYSTEM_KERNEL_MODULES := $(wildcard $(SYSTEM_DLKM_MODULES_PATH)/*.ko)
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(patsubst %,$(SYSTEM_DLKM_MODULES_PATH)/%,$(shell cat $(SYSTEM_DLKM_MODULES_PATH)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES := $(wildcard $(DLKM_MODULES_PATH)/*.ko)
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(patsubst %,$(DLKM_MODULES_PATH)/%,$(shell cat $(DLKM_MODULES_PATH)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DLKM_MODULES_PATH)/modules.blocklist
+
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES := $(wildcard $(RAMDISK_MODULES_PATH)/*.ko)
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(patsubst %,$(RAMDISK_MODULES_PATH)/%,$(shell cat $(RAMDISK_MODULES_PATH)/modules.load))
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD  := $(patsubst %,$(RAMDISK_MODULES_PATH)/%,$(shell cat $(RAMDISK_MODULES_PATH)/modules.load.recovery))
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_BLOCKLIST_FILE := $(RAMDISK_MODULES_PATH)/modules.blocklist
 
 # Partitions
 BOARD_MOT_DP_GROUP_SIZE := 7511998464 # (BOARD_SUPER_PARTITION_SIZE - 4MB)
